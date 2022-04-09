510d7e951370a467c6cb62abcca0ac776f7aa892
Aleksandr Elmekeev <donamok@gmail.com>
2022-04-09 02:50:01 +0100

    Add sorting to build command

--- utils/build.sh
+++ utils/build.sh
@@ -6,6 +6,10 @@ temp=./utils/temp.json.tmp
 for filename in ./csv/*.csv; do
   city=$(basename "$filename" .csv)
 
+  # sort
+  echo -ne "year;latitude;longitude\n$(tail -n +2 $filename | sort)" > $filename
+
+  # generate temporary json files for each city
   cat "./csv/$city.csv" | jq -sRr "split(\"\\n\") | .[1:] | map(split(\";\")) | map({(.[0]): [.[1], .[2]] | join(\",\") }) | add as \$points | {\"$city\": {\"points\": \$points}}" >$temp
   jq -s ".[0] * .[1] | {\"$city\": .[\"$city\"]}" ./utils/configs.json $temp >"./utils/$city.json.tmp"
 done
