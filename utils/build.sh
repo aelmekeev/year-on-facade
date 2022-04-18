ae4bf39aa85d1f83d00d2ad80d51d2cfa8562238
Alex Elmekeev <55494848+aelmekeev@users.noreply.github.com>
2022-04-18 19:44:14 +0100

    Refactor to have different data files per city (#5)

--- utils/build.sh
+++ utils/build.sh
@@ -12,12 +12,10 @@ for filename in ./csv/*.csv; do
   # generate temporary json files for each city
   cat "./csv/$city.csv" | jq -sRr "split(\"\\n\") | .[1:] | map(split(\";\")) | map({(.[0]): [.[1], .[2]] | join(\",\") }) | add as \$points | {\"$city\": {\"points\": \$points}}" >$temp
   jq -s ".[0] * .[1] | {\"$city\": .[\"$city\"]}" ./utils/configs.json $temp >"./utils/$city.json.tmp"
-done
 
-rm $temp
-
-cat >./js/data.js <<EOF
-const data = $(cat ./utils/*.json.tmp | jq -s '. | add')
+  cat >"./js/$city.js" <<EOF
+const data = $(cat ./utils/$city.json.tmp | jq ".[\"$city\"]")
 EOF
+done
 
 rm ./utils/*.tmp
