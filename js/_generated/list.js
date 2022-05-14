799dac0613f36cf4c2b621d940817e853d0b0c2b
Aleksandr Elmekeev <donamok@gmail.com>
2022-05-14 19:33:39 +0100

    London 14.05

--- js/_generated/list.js
+++ js/_generated/list.js
@@ -1,9 +1,9 @@
 const data = {
   "Billund": 1,
   "Copenhagen": 3,
-  "London": 20,
+  "London": 27,
   "Perm": 1,
-  "TODO": 57,
-  "World": 69,
+  "TODO": 52,
+  "World": 70,
 }
 const minYear = 1709;
