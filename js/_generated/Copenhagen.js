0080430a9e5b4423f9538e22f04649fca08dcc6c
Aleksandr Elmekeev <donamok@gmail.com>
2022-06-06 21:54:15 +0100

    06.06.2022. Add Copenhagen hiretage register

--- js/_generated/Copenhagen.js
+++ js/_generated/Copenhagen.js
@@ -12,6 +12,10 @@ const data = {
       "west": 12.4526044,
       "east": 12.6514573
     },
+    "external": {
+      "label": "Check out Fredede og Bevaringsværdige Bygninger",
+      "template": "https://www.kulturarv.dk/fbb/bygningvis.pub?bygning=EXTERNAL_ID"
+    },
     "useInternalMap": true,
     "photosBaseUrl": "https://year-on-facade.s3.eu-west-2.amazonaws.com",
     "city": "Copenhagen"
@@ -21,19 +25,25 @@ const data = {
       "latlng": {
         "lat": 55.679587967436206,
         "lng": 12.59258735833644
-      }
+      },
+      "notes": "",
+      "external": "3177920"
     },
     "1899": {
       "latlng": {
         "lat": 55.67751919534578,
         "lng": 12.536597484595521
-      }
+      },
+      "notes": "",
+      "external": "3343222"
     },
     "1906": {
       "latlng": {
         "lat": 55.69863940860437,
         "lng": 12.540628344999996
-      }
+      },
+      "notes": "",
+      "external": "3163917"
     }
   }
 }
--- js/_generated/list.js
+++ js/_generated/list.js
@@ -1,9 +1,9 @@
 const data = {
   "Billund, Denmark": 1,
   "Copenhagen, Denmark": 3,
-  "London, UK": 91,
+  "London, UK": 92,
   "Perm, Russia": 1,
-  "TODO": 50,
+  "TODO": 49,
   "World": 135,
 }
 const minYear = 1669;
