f12e03ae1a68f631f94dfd1199e3a5107e54cc80
Alex Elmekeev <55494848+aelmekeev@users.noreply.github.com>
2022-06-11 19:45:09 +0100

    11.06.2022 (#49)

--- js/_generated/list.js
+++ js/_generated/list.js
@@ -1,9 +1,9 @@
 const data = {
   "Billund, Denmark": 1,
   "Copenhagen, Denmark": 3,
-  "London, UK": 92,
+  "London, UK": 103,
   "Perm, Russia": 1,
-  "TODO": 56,
-  "World": 142,
+  "TODO": 44,
+  "World": 141,
 }
 const minYear = 1669;
