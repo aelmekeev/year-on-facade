e840f8eb66444be3d1e1d404a3560553e37a5db7
Alex Elmekeev <55494848+aelmekeev@users.noreply.github.com>
2022-06-27 22:43:33 +0100

    Add century milestones to the timeline (#58)

--- js/list.js
+++ js/list.js
@@ -1,47 +1,65 @@
 const sortByCount = ([, a], [, b]) => b - a;
 const sortAlphabetically = ([a,], [b,]) => b < a;
 
-function sortList(sortFunction = sortByCount) {
-  const parent = document.querySelector("#list");
-  parent.innerHTML = "";
+function addTimeline(list) {
+  const timeline = document.createElement('div')
+  timeline.id = 'timeline'
 
-  const row = document.createElement('div');
-  row.classList.add('row');
-  parent.appendChild(row);
+  const currentYear = new Date().getFullYear()
+  const range = currentYear - minYear
+  let start = minYear
+  let end = start
+  while (start + 100 < currentYear) {
+    end = Math.floor((start + 100) / 100) * 100
+    length = (end - start) * 100 / range
+    const century = document.createElement('div')
+    century.style.width = `${length}%`
+    timeline.appendChild(century)
+    start = end
+  }
+
+  list.appendChild(timeline)
+}
+
+function sortList(sortFunction = sortByCount) {
+  const parent = document.querySelector('#list')
+  parent.innerHTML = ''
+
+  addTimeline(parent)
 
   Object.entries(data)
     .sort(sortFunction)
     .forEach(e => {
-      const title = e[0]; 
-      const city = title.split(',')[0];
-      const score = e[1];
+      const title = e[0]
+      const city = title.split(',')[0]
+      const score = e[1]
 
-      const row = document.createElement('div');
-      row.classList.add('row');
-      row.innerHTML = `<a href="${window.location.href.replace(/(year-on-facade[^\/]+)/, '$1/stats')}?city=${city}">${title}</a> - ${score}`;
-      row.style.backgroundImage = `url("img/_generated/${city}.svg")`;
-      parent.appendChild(row);
+      const row = document.createElement('div')
+      row.classList.add('row')
+      row.innerHTML = `<a href="${window.location.href.replace(/(year-on-facade[^//]*)/, '$1/stats')}?city=${city}">${title}</a> - ${score}`
+      row.style.backgroundImage = `url("img/_generated/${city}.svg")`
+      parent.appendChild(row)
     });
 }
 
 function sortListListener(sortBy) {
-  const sortFunction = sortBy == "by_count" ? sortByCount : sortAlphabetically;
+  const sortFunction = sortBy == 'by_count' ? sortByCount : sortAlphabetically
 
   return function () {
-    sortList(sortFunction);
+    sortList(sortFunction)
   }
 }
 
 function updateMarks() {
-  document.querySelector("#start").innerText = minYear;
-  document.querySelector("#end").innerText = new Date().getFullYear();
+  document.querySelector('#start').innerText = minYear
+  document.querySelector('#end').innerText = new Date().getFullYear()
 }
 
 function updateList() {
-  sortList();
-  document.querySelectorAll("[name=sort]")
-    .forEach(r => r.onclick = sortListListener(r.value));
-  updateMarks();
+  sortList()
+  document.querySelectorAll('[name=sort]')
+    .forEach(r => r.onclick = sortListListener(r.value))
+  updateMarks()
 }
 
 window.onload = () => updateList()
