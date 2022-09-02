bc208d9498cc845c95bbc09dce4a18b6f1b59b17
Aleksandr Elmekeev <donamok@gmail.com>
2022-09-03 00:57:11 +0100

    Add Brighton and Hove

---
+++ js/_generated/Brighton_and_Hove.js
@@ -0,0 +1,73 @@
+const data = {
+  "config": {
+    "country": "UK",
+    "zoom": 10,
+    "center": {
+      "lat": 50.8396,
+      "lng": -0.134
+    },
+    "borders": {
+      "south": 50.797214,
+      "north": 50.892159,
+      "west": -0.240598,
+      "east": -0.017147
+    },
+    "external": {
+      "label": "Check out Historic England",
+      "template": "https://historicengland.org.uk/listing/the-list/list-entry/EXTERNAL_ID?section=official-list-entry"
+    },
+    "useInternalMap": true,
+    "photosBaseUrl": "https://year-on-facade.s3.eu-west-2.amazonaws.com",
+    "city": "Brighton_and_Hove"
+  },
+  "points": {
+    "1841": {
+      "latlng": {
+        "lat": 50.82436384784516,
+        "lng": -0.15388839369851492
+      },
+      "notes": "",
+      "external": "1379976"
+    },
+    "1846": {
+      "latlng": {
+        "lat": 50.81725443541366,
+        "lng": -0.11749110475440262
+      },
+      "notes": "",
+      "external": "1379993"
+    },
+    "1868": {
+      "latlng": {
+        "lat": 50.82728137001251,
+        "lng": -0.1409624150127564
+      },
+      "notes": "",
+      "external": ""
+    },
+    "1891": {
+      "latlng": {
+        "lat": 50.81912217271996,
+        "lng": -0.1239040074582801
+      },
+      "notes": "",
+      "external": ""
+    },
+    "1896": {
+      "latlng": {
+        "lat": 50.82621033726481,
+        "lng": -0.1420692953514744
+      },
+      "notes": "",
+      "external": ""
+    },
+    "1914": {
+      "latlng": {
+        "lat": 50.82096548106133,
+        "lng": -0.13455930638858804
+      },
+      "notes": "",
+      "external": ""
+    }
+  }
+}
--- js/_generated/list.js
+++ js/_generated/list.js
@@ -1,10 +1,11 @@
 const data = {
   "Billund, Denmark": 1,
+  "Brighton_and_Hove, UK": 6,
   "Copenhagen, Denmark": 3,
   "London, UK": 171,
   "Perm, Russia": 1,
   "Replacements": 4,
   "TODO": 17,
-  "World": 173,
+  "World": 174,
 }
 const minYear = 1607;
