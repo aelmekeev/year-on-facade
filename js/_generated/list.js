beb617f1ae6a8e6a2dd177ea384b91e588c6c17b
Aleksandr Elmekeev <donamok@gmail.com>
2023-01-15 22:15:31 +0000

    15.01.2023_snoop

--- js/_generated/list.js
+++ js/_generated/list.js
@@ -4,7 +4,7 @@ const data = [
   {name: "Brighton_and_Hove", country: "UK", count: 6, minYear: 1841},
   {name: "Copenhagen", country: "Denmark", count: 3, minYear: 1756},
   {name: "Denmark", country: "null", count: 4, minYear: 1756},
-  {name: "London", country: "UK", count: 211, minYear: 1580},
+  {name: "London", country: "UK", count: 212, minYear: 1580},
   {name: "Manchester", country: "UK", count: 3, minYear: 1832},
   {name: "Perm", country: "Russia", count: 1, minYear: 2010},
   {name: "Russia", country: "null", count: 1, minYear: 2010},
