19819291f8aa5e25195983966a5d0fb2986bbe58
Aleksandr Elmekeev <donamok@gmail.com>
2023-09-15 11:40:02 +0100

    Cost optimisation for original photos

--- photos/upload.sh
+++ photos/upload.sh
@@ -5,8 +5,9 @@ set -euo pipefail
 aws_profile="year-on-facade"
 photos_dir="./photos/original"
 
-# read bucket name from config.properties file
 s3_bucket="$(grep '^bucket' config.properties | cut -d'=' -f2 | cut -d'#' -f1 | cut -d'"' -f2)-photos"
 s3_folder="s3://$s3_bucket/original"
 
-aws-vault exec $aws_profile -- aws s3 sync $photos_dir $s3_folder --exclude "*" --include "*.jpg"
+s3_storage_class="$(grep '^s3_storage_class' config.properties | cut -d'=' -f2 | cut -d'#' -f1 | cut -d'"' -f2)"
+
+aws-vault exec $aws_profile -- aws s3 sync $photos_dir $s3_folder --storage-class $s3_storage_class --exclude "*" --include "*.jpg"
