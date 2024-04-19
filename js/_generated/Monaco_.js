4b162b0ba6d274db4b82d43288d76fb8862a7e33
Alex Elmekeev <55494848+aelmekeev@users.noreply.github.com>
2024-04-19 22:58:59 +0100

    Add buildings from Monaco (#152)

--- js/_generated/Monaco_.js
+++ js/_generated/Monaco_.js
@@ -1,81 +1,21 @@
 const data = {
   "config": {
     "borders": {
-      "south": 41.363662,
-      "north": 51.075319,
-      "west": -4.796351,
-      "east": 9.567461
-    },
-    "external": {
-      "label": "Check out Base Mérimée",
-      "template": "https://www.pop.culture.gouv.fr/notice/merimee/EXTERNAL_ID"
+      "south": 43.724594,
+      "north": 43.751923,
+      "west": 7.409104,
+      "east": 7.439848
     },
     "useInternalMap": true,
     "photosBaseUrl": "https://year-on-facade.s3.eu-west-2.amazonaws.com/web",
     "whatIsThisLink": "./what-is-this"
   },
   "points": {
-    "1648": {
-      "city": "Nice",
+    "1900": {
+      "city": "Monaco",
       "latlng": {
-        "lat": 43.69804150842751,
-        "lng": 7.2767927399627546
-      },
-      "notes": ""
-    },
-    "1869": {
-      "city": "Nice",
-      "latlng": {
-        "lat": 43.70023806187411,
-        "lng": 7.28534807075022
-      },
-      "notes": ""
-    },
-    "1877": {
-      "city": "Menton",
-      "latlng": {
-        "lat": 43.774709532337496,
-        "lng": 7.500799511495009
-      },
-      "notes": ""
-    },
-    "1882": {
-      "city": "Nice",
-      "latlng": {
-        "lat": 43.69969094664279,
-        "lng": 7.268437193756377
-      },
-      "notes": ""
-    },
-    "1897": {
-      "city": "Nice",
-      "latlng": {
-        "lat": 43.70061135322727,
-        "lng": 7.2714094262081455
-      },
-      "notes": ""
-    },
-    "1903": {
-      "city": "Nice",
-      "latlng": {
-        "lat": 43.70742877886892,
-        "lng": 7.2622871620867935
-      },
-      "notes": ""
-    },
-    "1909": {
-      "city": "Menton",
-      "latlng": {
-        "lat": 43.77445080628653,
-        "lng": 7.496384817131317
-      },
-      "notes": ""
-    },
-    "1913": {
-      "city": "Menton",
-      "latlng": {
-        "lat": 43.77346610951384,
-        "lng": 7.495096287558734
+        "lat": 43.74351326592167,
+        "lng": 7.4275865307354545
       },
       "notes": ""
     }
--- js/_generated/Turkey.js
+++ js/_generated/Turkey.js
@@ -96,6 +96,9 @@ const data = {
         }
       }
     },
+    "Monaco_": {
+      "config": {}
+    },
     "Portugal": {
       "config": {
         "external": {
@@ -467,6 +470,12 @@ const data = {
           "label": "Check out Base Mérimée",
           "template": "https://www.pop.culture.gouv.fr/notice/merimee/EXTERNAL_ID"
         }
+      }
+    },
+    "Monaco": {
+      "config": {
+        "country": "Monaco_",
+        "external": null
       }
     },
     "Perm": {
