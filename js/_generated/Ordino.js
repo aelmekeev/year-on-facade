6985a4fac05b9f6fe1bea6fb0e24622a4e07fb74
Aleksandr Elmekeev <donamok@gmail.com>
2026-04-10 07:28:40 +0100

    04.2026 Andorra

---
+++ js/_generated/Ordino.js
@@ -0,0 +1,37 @@
+const data = {
+  "config": {
+    "country": "Andorra",
+    "borders": {
+      "south": 42.552369,
+      "north": 42.559705,
+      "west": 1.531372,
+      "east": 1.537529
+    },
+    "useInternalMap": true,
+    "photosBaseUrl": "https://year-on-facade.s3.eu-west-2.amazonaws.com/web",
+    "city": "Ordino"
+  },
+  "points": {
+    "1849": {
+      "latlng": {
+        "lat": 42.55657356048789,
+        "lng": 1.533891429463631
+      },
+      "notes": ""
+    },
+    "1996": {
+      "latlng": {
+        "lat": 42.5566927520679,
+        "lng": 1.5336621828620554
+      },
+      "notes": ""
+    },
+    "1998": {
+      "latlng": {
+        "lat": 42.55661167828672,
+        "lng": 1.5327900168887778
+      },
+      "notes": ""
+    }
+  }
+}
